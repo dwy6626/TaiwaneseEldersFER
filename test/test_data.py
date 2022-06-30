b9af5ee529105b0151af571faf7c775ac664a6e9
De-Wei Ye <david@dwye.dev>
2022-06-30 22:14:03 +0800

    add unit test

--- test/test_data.py
+++ test/test_data.py
@@ -10,26 +10,31 @@ class TestData(unittest.TestCase):
         self.old_size = 293
         self.young_size = 334
 
+    @unittest.skipUnless(tw_face.data_exist(), 'tw face data not exist')
     def test_old_count(self):
         x, y = tw_face.read_data('old')
         self.assertEqual(len(x), self.old_size)
         self.assertEqual(y.shape, (self.old_size, ))
 
+    @unittest.skipUnless(tw_face.data_exist(), 'tw face data not exist')
     def test_young_count(self):
         x, y = tw_face.read_data('young')
         self.assertEqual(len(x), self.young_size)
         self.assertEqual(y.shape, (self.young_size, ))
 
+    @unittest.skipUnless(tw_face.data_exist(), 'tw face data not exist')
     def test_all_count(self):
         x, y = tw_face.read_data()
         self.assertEqual(len(x), self.young_size + self.old_size)
         self.assertEqual(y.shape, (self.young_size + self.old_size, ))
 
+    @unittest.skipUnless(tw_face.data_exist(), 'tw face data not exist')
     def test_readable(self):
         x, y = tw_face.read_data()
         for xx in x:
             Image.open(xx).close()
 
+    @unittest.skipUnless(tw_face.data_exist(), 'tw face data not exist')
     def test_label(self):
         x, y = tw_face.read_data()
         for yy in y.flat:        
